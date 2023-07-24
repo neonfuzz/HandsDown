1e1c5ff6fd6e658dbfa2dceaaf174760d4ea2339
neonfuzz <addie.kingsland@gmail.com>
2023-07-23 19:32:23 -0700

    Fixing backspace bug
    - holding backspace sends it repeatedly
    - SHIFT+BKSPC = DEL

--- backspace.c
+++ backspace.c
@@ -1,14 +1,12 @@
 static uint8_t backspace_amt = 1;
 
 bool process_backspace_user(uint16_t keycode, const keyrecord_t *record) {
-    bool    return_state = true;
     uint8_t i;
     if (keycode == KC_BSPC && record->event.pressed) {
-        for (i = 1; i <= backspace_amt; ++i) {
+        for (i = 1; i < backspace_amt; ++i) {
             tap_code(KC_BSPC);
         }
-        return_state = false;
     }
     backspace_amt = 1;
-    return return_state;
+    return true;
 }
