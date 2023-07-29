19aaf05f68f689a10f9d314672c6cc8acdffb920
neonfuzz <addie.kingsland@gmail.com>
2023-07-29 11:28:32 -0700

    Bugfix from `adaptive`
    `keycode` was being `&`ed with `0xFF` which was cutting off
    important contextual information; keycodes from e.g.,
    unicode indices, layer indices were being caught in the
    matrix scan.

--- adaptive.c
+++ adaptive.c
@@ -17,15 +17,10 @@ bool process_adaptive_user(uint16_t keycode, const keyrecord_t *record) {
             // Don't do anything if mods are present.
             return true;
         }
-        if (keycode >= QK_UNICODEMAP) {
-            // Don't do anything if we're in unicode mode.
-            return true;
-        }
         if (timer_elapsed32(prior_keydown) < ADAPTIVE_TERM) {
             return_state    = true;
             uint16_t first  = KC_NO;
             uint16_t second = KC_NO;
-            keycode         = keycode & 0xFF;
             clear_mods();
 
             switch (prior_keycode) {
